5e5048aa4049c8db1bd1b40b363fff93d9958b94
alannyled <84908245+alannyled@users.noreply.github.com>
2024-09-09 14:16:06 +0200

    Db connection OK

---
+++ RundownDbService/Data/DbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RundownDbService.Data
+{
+    public class RundownDbContext : DbContext
+    {
+        public RundownDbContext(DbContextOptions<RundownDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Template> Templates { get; set; } // Repræsenterer en tabel i databasen
+    }
+}
---
+++ RundownDbService/Data/Template.cs
@@ -0,0 +1,8 @@
+﻿namespace RundownDbService.Data
+{
+    public class Template
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}

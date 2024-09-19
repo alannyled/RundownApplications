1979a74f09f4fabe11cc90485b846f0f8ffd83f3
alannyled <84908245+alannyled@users.noreply.github.com>
2024-09-19 22:18:22 +0200

    flere callbacks og logik

---
+++ RundownEditorCore/DTO/RundownObjects.cs
@@ -0,0 +1,11 @@
+﻿namespace RundownEditorCore.DTO
+{
+    public class RundownObjects
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Duration { get; set; }
+        public string Category { get; set; }
+
+    }
+}

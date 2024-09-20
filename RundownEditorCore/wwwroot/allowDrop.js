1b42aa6ab18b32e336395887044ce76abfc809f5
alannyled <84908245+alannyled@users.noreply.github.com>
2024-09-20 15:00:51 +0200

    indikator af dropdown område

--- RundownEditorCore/wwwroot/allowDrop.js
+++ RundownEditorCore/wwwroot/allowDrop.js
@@ -1,3 +1,20 @@
 ﻿function allowDrop(event) {
+    event.target.closest("tr").style.borderTop = "5px solid rgba(0,0,255,0.2)";
     event.preventDefault();
+}
+
+function leaveDrop(event) {
+    event.target.closest("tr").style.borderTop = "none";
+}
+
+function endDrop(event) {
+
+    const tbody = event.target.closest("tbody");
+
+    if (tbody) {
+        const rows = tbody.querySelectorAll("tr");
+        rows.forEach(row => {
+            row.style.borderTop = "none";
+        });
+    }
 }

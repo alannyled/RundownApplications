5982a125ffcc7d7017f378242af51b057b841481
alannyled <84908245+alannyled@users.noreply.github.com>
2024-09-25 19:57:20 +0200

    opdeling af modal til komponent. plus sekunder i input Time

--- RundownEditorCore/DTO/RundownObjects.cs
+++ RundownEditorCore/DTO/RundownObjects.cs
@@ -4,7 +4,7 @@
     {
         public string Id { get; set; }
         public string Name { get; set; }
-        public string Duration { get; set; }
+        public TimeOnly Duration { get; set; }
         public string Category { get; set; }
 
     }

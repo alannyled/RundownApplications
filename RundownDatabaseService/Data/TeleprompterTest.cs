e165d38d205d19b5c323dfc0fcd195119fe2df24
alannyled <84908245+alannyled@users.noreply.github.com>
2024-10-08 22:28:57 +0200

    Slettet gammel Database service

--- RundownDatabaseService/Data/TeleprompterTest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace RundownDbService.Data
-{
-    public class TeleprompterTest
-    {
-        public int TeleprompterTestId { get; set; }
-        public string Text { get; set; }
-    }
-}

d84205e5b20ac03ddb174ea4a33b040c3e028057
alannyled <84908245+alannyled@users.noreply.github.com>
2024-10-08 15:48:36 +0200

    opryd

--- RundownEditorCore/DTO/RundownDTO.cs
+++ RundownEditorCore/DTO/RundownDTO.cs
@@ -4,8 +4,8 @@
     {
         public Guid Uuid { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }
-        public string Type{ get; set; }
+        public string? Description { get; set; }
+        public string? Type{ get; set; }
         public DateTime BroadcastDate { get; set; }
         public DateTime? ArchivedDate { get; set; } = null;
         public string? ArchivedBy { get; set; } = null;

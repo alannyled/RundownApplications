e09a718d3a1de4b030c97d86ac01b59427b4e31d
alannyled <84908245+alannyled@users.noreply.github.com>
2024-10-11 08:32:32 +0200

    minor error

--- RundownEditorCore/DTO/RundownItemDTO.cs
+++ RundownEditorCore/DTO/RundownItemDTO.cs
@@ -5,7 +5,7 @@ namespace RundownEditorCore.DTO
     public class RundownItemDTO
     {       
         public Guid UUID { get; set; }
-        public Guid RundownId { get; set; }
+        public string RundownId { get; set; }
         public string Name { get; set; }
         public string Duration { get; set; }
         public int Order { get; set; }

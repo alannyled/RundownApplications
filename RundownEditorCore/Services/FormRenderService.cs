dd4b88235b0374b0153ce65137c13dd9e7d6457c
alannyled <84908245+alannyled@users.noreply.github.com>
2024-10-26 22:16:39 +0200

    Callbacks ændret til besked håndtering

--- RundownEditorCore/Services/FormRenderService.cs
+++ RundownEditorCore/Services/FormRenderService.cs
@@ -8,31 +8,25 @@ namespace RundownEditorCore.Services
     public class FormRenderService
     {
         public RenderFragment RenderItemDetailForm(
-            string DetailType,
-            EventCallback<RundownDTO> onCreateCallback
+            string DetailType
             )
         {
             return builder =>
             {
                 builder.OpenComponent(0, typeof(ItemDetailForm));
-                builder.AddAttribute(2, "DetailType", DetailType);
-                builder.AddAttribute(3, "OnItemDetailCreated", EventCallback.Factory.Create<RundownDTO>(this, onCreateCallback));
+                builder.AddAttribute(1, "DetailType", DetailType);
                 builder.CloseComponent();
             };
         }
 
         public RenderFragment RenderRundownItemForm(
-            List<string> Templates,
-            RundownDTO SelectedRundown,
-            EventCallback<RundownDTO> onCreateCallback
+            List<string> Templates
             )
         {
             return builder =>
             {
                 builder.OpenComponent(0, typeof(RundownItemForm));
-                builder.AddAttribute(1, "SelectedRundown", SelectedRundown);
-                builder.AddAttribute(2, "Templates", Templates);
-                builder.AddAttribute(3, "OnItemCreated", EventCallback.Factory.Create<RundownDTO>(this, onCreateCallback));
+                builder.AddAttribute(1, "Templates", Templates);
                 builder.CloseComponent();
             };
         }
@@ -40,8 +34,7 @@ namespace RundownEditorCore.Services
         public RenderFragment RenderNewRundownForm(
             List<TemplateDTO> AllTemplates, 
             List<ControlRoomDTO> ControlRooms, 
-            DateTime BroadcastDate, 
-            EventCallback<CreateRundownResultDTO> onCreateCallback
+            DateTime BroadcastDate
             )
         {
             return builder =>
@@ -50,7 +43,6 @@ namespace RundownEditorCore.Services
                 builder.AddAttribute(1, "Templates", AllTemplates);
                 builder.AddAttribute(2, "ControlRooms", ControlRooms);
                 builder.AddAttribute(3, "BroadcastDate", BroadcastDate);
-                builder.AddAttribute(4, "OnRundownCreated", EventCallback.Factory.Create<CreateRundownResultDTO>(this, onCreateCallback));
                 builder.CloseComponent();
             };
         }

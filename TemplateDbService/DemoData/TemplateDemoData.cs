130174360d6053721af2b53cde02ae71211993b3
alannyled <84908245+alannyled@users.noreply.github.com>
2024-11-19 19:10:40 +0100

    Virker nu (?)

--- TemplateDbService/DemoData/TemplateDemoData.cs
+++ TemplateDbService/DemoData/TemplateDemoData.cs
@@ -6,25 +6,14 @@ namespace TemplateDbService.DemoData
     {
         public static List<RundownTemplate> TemplateData()
         {
-            DateTime now = DateTime.Now;
-            var Rundown1Uuid = Guid.NewGuid();
-            var Rundown2Uuid = Guid.NewGuid();
-            var Rundown3Uuid = Guid.NewGuid();
-            var Rundown4Uuid = Guid.NewGuid();
-            var Rundown5Uuid = Guid.NewGuid();
-            var Item1Uuid = Guid.NewGuid();
-            var Item2Uuid = Guid.NewGuid();
-            var Item3Uuid = Guid.NewGuid();
-            var Item4Uuid = Guid.NewGuid();
-            var Item5Uuid = Guid.NewGuid();
-
             var templates = new List<RundownTemplate>
                 {
                     new() { UUID = Guid.NewGuid() ,Name = "TVA 1830", CreatedDate = DateTime.Now, ArchivedDate = null, Items = [] },                    
                     new() { UUID = Guid.NewGuid() ,Name = "21 Søndag", CreatedDate = DateTime.Now, ArchivedDate = null, Items = [] },
                     new() { UUID = Guid.NewGuid() ,Name = "Time Nyheder", CreatedDate = DateTime.Now, ArchivedDate = null, Items = [] },
                     new() { UUID = Guid.NewGuid() ,Name = "Deadline", CreatedDate = DateTime.Now, ArchivedDate = null, Items = [] },
-                    new() { UUID = Guid.NewGuid() ,Name = "Krigens døgn", CreatedDate = DateTime.Now, ArchivedDate = null, Items = [] }
+                    new() { UUID = Guid.NewGuid() ,Name = "Krigens døgn", CreatedDate = DateTime.Now, ArchivedDate = null, Items = [] },
+                    new() { UUID = Guid.NewGuid() ,Name = "Tom skabelon", CreatedDate = DateTime.Now, ArchivedDate = null, Items = [] }
                 };
             return templates;
         }

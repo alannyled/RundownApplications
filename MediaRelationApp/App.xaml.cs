b66c2c4031b68b944325e7f317c9f0b5c6473741
alannyled <84908245+alannyled@users.noreply.github.com>
2024-11-23 21:14:48 +0100

    Ny Maui app

---
+++ MediaRelationApp/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace MediaRelationApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+    }
+}
---
+++ MediaRelationApp/MauiProgram.cs
@@ -0,0 +1,56 @@
+﻿using MediaRelationApp.Services;
+using Microsoft.Extensions.Logging;
+using Microsoft.Maui.LifecycleEvents;
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Windows.Graphics;
+using WinRT.Interop;
+
+namespace MediaRelationApp
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+
+            builder.Services.AddMauiBlazorWebView();
+            builder.Services.AddSingleton<ApiService>();
+#if WINDOWS
+            builder.ConfigureLifecycleEvents(events =>
+            {
+                events.AddWindows(windows =>
+                {
+                    windows.OnWindowCreated(window =>
+                    {
+                        var nativeWindow = window as Microsoft.UI.Xaml.Window;
+                        if (nativeWindow != null)
+                        {
+                            // Brug AppWindow til at ændre vinduets størrelse
+                            var appWindow = AppWindow.GetFromWindowId(Win32Interop.GetWindowIdFromWindow(WindowNative.GetWindowHandle(nativeWindow)));
+
+                            if (appWindow != null)
+                            {
+                                appWindow.Resize(new SizeInt32(1800, 1200));
+                                appWindow.Move(new PointInt32(200, 100));
+                            }
+                        }
+                    });
+                });
+            });
+#endif
+#if DEBUG
+            builder.Services.AddBlazorWebViewDeveloperTools();
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}

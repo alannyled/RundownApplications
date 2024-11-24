72c4a430a4a3f07badc3d9555e2b4fcfbad569ff
alannyled <84908245+alannyled@users.noreply.github.com>
2024-11-24 14:52:30 +0100

    rebuild MR App

---
+++ MediaRelationApp2/MauiProgram.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Logging;
+using MediaRelationApp2.Services;
+
+
+namespace MediaRelationApp2
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+
+            builder.Services.AddMauiBlazorWebView();
+            builder.Services.AddSingleton<ApiService>();
+#if DEBUG
+            builder.Services.AddBlazorWebViewDeveloperTools();
+            builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
---
+++ MediaRelationApp2/Services/ApiService.cs
@@ -0,0 +1,42 @@
+﻿using MediaRelationApp2.DTO;
+using Newtonsoft.Json;
+using System.Net.Http.Json;
+
+namespace MediaRelationApp2.Services
+{
+    public class ApiService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ApiService()
+        {
+            _httpClient = new HttpClient();
+            // _httpClient.BaseAddress = new Uri("https://localhost:3010/api/Rundown"); 
+        }
+
+        // GET: Hent en liste af rundowns
+        public async Task<List<RundownDTO>> GetRundownsAsync()
+        {
+            var response = await _httpClient.GetAsync("https://localhost:3010/api/Rundown/fetch-rundowns-with-controlrooms");
+            if (response.IsSuccessStatusCode)
+            {
+                var json = await response.Content.ReadAsStringAsync();
+                var rundowns = JsonConvert.DeserializeObject<List<RundownDTO>>(json);
+                return rundowns ?? [];
+            }
+            return [];
+        }
+
+
+        public async Task<RundownDTO?> UpdateDetailAsync(string rundownId, DetailDTO itemDetail)
+        {
+            var response = await _httpClient.PutAsJsonAsync($"https://localhost:3010/api/Rundown/update-detail-in-story/{rundownId}", itemDetail);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadFromJsonAsync<RundownDTO>();
+            }
+            return null;
+        }
+    }
+}
